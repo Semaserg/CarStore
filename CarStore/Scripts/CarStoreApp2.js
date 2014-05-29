fc34e107df63ce573d8fa420c7ef27c3aa434b43
Sergey <Semaserg@gmail.com>
2014-05-30 01:18:59 +0300

    added react views
    Added react engine and first react views

---
+++ CarStore/Scripts/CarStoreApp2.js
@@ -0,0 +1,189 @@
+﻿/** @jsx React.DOM */
+CarStoreApp = new Backbone.Marionette.Application();
+
+CarStoreApp.addRegions({
+    searchResultsRegion: '#search-results-container',
+    searchForsm: '#search-form'
+});
+
+Car = Backbone.Model.extend({
+    defaults: {
+        Id: 0,
+        Speed: 100,
+        Title: 'Empty',
+        Rank: 0
+    },
+
+    rankUp: function () {
+        this.set('Rank', this.get('Rank') + 1);
+    },
+
+    rankDown: function () {
+        this.set('Rank', this.get('Rank') - 1);
+    }
+});
+
+CarCollection = Backbone.Collection.extend({
+    model: Car,
+
+    url: '../api/car',
+
+    initialize: function (cars) {
+
+        var self = this;
+
+        CarStoreApp.on('car:up', function (car) {
+            car.rankUp();
+            self.sort();
+            self.trigger('reset');
+            //alert('Up car with Title ' + car.attributes.Title);
+        });
+
+        CarStoreApp.on('car:down', function (car) {
+            car.rankDown();
+            self.sort();
+            self.trigger('reset');
+            //alert('Down car with Title ' + car.attributes.Title);
+        });
+
+        CarStoreApp.on('car:destroy', function (car) {
+            car.destroy();// destroy model
+            self.sort();
+            self.trigger('reset');
+            //alert('Down car with Title ' + car.attributes.Title);
+        });
+    },
+
+    comparator: function (car) {
+        return car.get('Rank');
+    }
+});
+
+CarView = Backbone.Marionette.ItemView.extend({
+    template: '#car-template',
+    tagName: 'li',
+    className: 'media',
+
+    events: {
+        'click .act-up': 'upBtnClick',
+        'click .act-down': 'downBtnClick',
+        'click .act-destroy': 'destroyBtnClick'
+    },
+    upBtnClick: function () {
+        // alert('Title is ' + this.model.Speed);
+        //var self = this;
+        CarStoreApp.trigger('car:up', this.model);
+    },
+    downBtnClick: function () {
+        CarStoreApp.trigger('car:down', this.model);
+    },
+    destroyBtnClick: function () {
+        CarStoreApp.trigger('car:destroy', this.model);
+    }
+
+});
+
+CarListView = Backbone.Marionette.CompositeView.extend({
+    template: '#car-list-template',
+    tagName: 'div',
+    className: 'panel panel-default',
+    id: 'carCollection',
+    itemView: CarView,
+
+    appendHtml: function (collectionView, itemView) {
+        collectionView.$('ul').append(itemView.el);
+    },
+
+    initialize: function () {
+        console.log("CarListView initialize start");
+
+        var self = this;
+
+        this.collection = new CarCollection();
+        this.collection.fetch({ reset: true });// it works with false too!!!! why???
+        //this.render(); // it works without render too.
+
+        CarStoreApp.on('search:fetch', function (searchText) {
+            self.collection.fetch({ reset: true, data: { searchText: searchText } });
+        });
+        console.log("CarListView initialize end");
+    }
+
+    //render : function() {
+    //    //console.log("CarListView render start");
+    //    //debugger;
+    //    //var a =this.collection;
+    //    //console.log("CarListView render end");
+    //}
+});
+
+AppRouter = Backbone.Router.extend({
+    routes: {
+        'search/:searchText': 'searchCar'
+    },
+
+    searchCar: function (searchText) {
+        CarStoreApp.trigger('search:fetch', searchText);
+    },
+
+    initialize: function () {
+
+        var self = this;
+
+        CarStoreApp.on('search:start', function (searchText) {
+            self.navigate('search/' + encodeURIComponent(searchText), { trigger: true, replace: false });
+        });
+    }
+});
+
+SearchView = Backbone.Marionette.ItemView.extend({
+    /*template: '#search-form-template',
+
+    /*events: {
+        'click .act-search': 'btnSearchClick'
+    },
+    */
+    btnSearchClick: function (data) {
+        console.log("btnSearchClick start. text = " + data.text);
+        CarStoreApp.trigger('search:start', data.text);
+    },
+
+    render: function () {
+        console.log("SearchView render start");
+        React.renderComponent(
+             <ReactSearchForm onSearchClick={this.btnSearchClick} />,
+    document.getElementById('search-container')
+       );
+console.log("SearchView render finish");
+}
+
+});
+
+
+
+CarStoreApp.addInitializer(function (options) {
+    console.log("CarStoreApp Initializer start");
+
+    React.renderComponent(<ReactHeader />, document.getElementById('header-container'));
+    React.renderComponent(<ReactCarGrid carList={[{Id : 5, Titile : "Volvo", Rank : 12, Speed : 450}]} />, document.getElementById('car-collection-container'));
+
+    var searchView = new SearchView();
+    CarStoreApp.searchForsm.show(searchView);
+
+    var carListView = new CarListView();
+    CarStoreApp.searchResultsRegion.show(carListView);
+    
+
+    var appRouter = new AppRouter();
+    //Backbone.history.start({ pushState: true, root:'home/index/' });
+
+    
+
+    Backbone.history.start();
+
+    console.log("CarStoreApp Initializer finish");
+});
+
+$(document).ready(function () {
+    CarStoreApp.start();
+});
---
+++ CarStore/Scripts/ReactWidgets.js
@@ -0,0 +1,81 @@
+﻿/** @jsx React.DOM */
+
+var ReactHeader = React.createClass({
+    render: function () {
+        return (
+            <div class="page-header">
+                <h1>Car Store App<small> find car of your dream</small></h1>
+            </div>
+        );
+    }
+});
+
+var ReactSearchForm = React.createClass({
+    searchClickHandler : function(){
+        console.log("ReactSearchForm searchClickHandler start");
+        var searchText = this.refs.searchText.getDOMNode().value.trim();
+        console.log("searchClickHandler, search text : " + searchText);
+        this.props.onSearchClick({text : searchText});
+    },
+
+    render: function(){
+        return (
+             <div class="container-fluid">
+                <div class="navbar-form navbar-left" role="search">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Search" ref="searchText" />
+                    </div>
+                    <a class="btn btn-default act-search" id="btnSearch" onClick={this.searchClickHandler}>Search</a>
+                </div>
+            </div>
+            );
+    }
+});
+
+var ReactCarGrid = React.createClass({
+    //getInitialState: function() {
+    //        return { 
+    //            carList: [] 
+    //        };
+    //    },
+    render: function () {
+        console.log("ReactCarGrid render start"); 
+        var rowList = this.props.carList.map(function(entity){
+                return <ReactCarGridRow carEntity={entity} />;
+           });
+        return (
+          <div>{rowList}</div>
+        );
+    }
+});
+
+var ReactCarGridRow = React.createClass({
+    render: function(){
+        return (
+            <div>
+        <a class="pull-left" href="#">
+           <img class="media-object" src="../Images/1401031633_camaro_256.png" alt="..." />
+       </a>
+       <div class="media-body">
+           <h4 class="media-heading">{this.props.carEntity.Title}</h4>
+           <div>Id: {this.props.carEntity.Id}</div>
+           <div>Rank: {this.props.carEntity.Rank}</div>
+           <div>Speed: {this.props.carEntity.Speed}</div>
+           <div>
+               <div class="btn-group btn-group-justified">
+                   <div class="btn-group">
+                       <button type="button" class="btn btn-default act-up">UP</button>
+                   </div>
+                   <div class="btn-group">
+                       <button type="button" class="btn btn-default act-destroy">Destroy</button>
+                   </div>
+                   <div class="btn-group">
+                       <button type="button" class="btn btn-default act-down">DOWN</button>
+                   </div>
+               </div>
+           </div>
+       </div>
+        </div>
+        );
+    }
+});
